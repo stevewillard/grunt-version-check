8b24ce4927eeab486586e8e81ee9f2ef6f498cd0
Steve Willard <stevewillard@gmail.com>
2014-08-26 23:20:36 -0400

    Async check updates of bower.json and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,34 @@
 /*
- * grunt-bower-version
- * https://github.com/stevewillard/grunt-bower-version
+ * grunt-version-check
+ * https://github.com/stevewillard/grunt-version-check
  *
  * Copyright (c) 2014 Steve Willard
  * Licensed under the MIT license.
  */
 
-'use strict';
-
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    bower_version: {
-      default_options: {
-        options: {
+    grunt.initConfig({
+        jshint: {
+            all: ["Gruntfile.js", "tasks/**/*.js"],
+            options: {
+                jshintrc: ".jshintrc"
+            }
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+
+        versioncheck : {
+            options : {
+                skip : ["semver"]
+            }
         }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
+    });
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    // Load this plugin
+    grunt.loadTasks("tasks");
 
-  });
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks("grunt-contrib-jshint");
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'bower_version', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
+    // By default, lint and run the task.
+    grunt.registerTask("default", ["jshint", "versioncheck"]);
 };
---
+++ tasks/version-check-grunt.js
@@ -0,0 +1,130 @@
+/*
+ * grunt-version-check
+ * https://github.com/stevewillard/grunt-version-check
+ *
+ * Copyright (c) 2014 Steve Willard
+ * Licensed under the MIT license.
+ */
+
+var bower = require("bower"),
+    npm = require("npm"),
+    _ = require("lodash"),
+    semver = require("semver"),
+    async = require("async");
+
+require("colors");
+
+/*
+   Takes either a bower.json or package.json and returns some metadata about each object
+   
+   Input: (bower.json)
+   { name: "my-bower-component", dependencies: {"foo" : "1.2.3"}, devDependencies: {"bar" : "4.5.6"} }
+
+   Output:
+    [{
+        "name": "foo",
+        "type": "bower",
+        "version": "1.2.3"
+    },{
+        "name": "bar",
+        "type": "bower",
+        "version": "4.5.6"
+    }]
+ */
+function componentFileToMetadatas(type, json) {
+    var results = [],
+        allDeps = _.merge(json.dependencies || {}, json.devDependencies || {});
+
+    _.each(Object.keys(allDeps), function(dependency) {
+        results.push({
+            "name": dependency,
+            "type": type,
+            "version": allDeps[dependency]
+        });
+    });
+
+    return results;
+}
+
+function readFile(grunt, file) {
+    return grunt.file.exists(file) ? grunt.file.readJSON(file) : {};
+}
+
+module.exports = function(grunt) {
+
+    grunt.registerTask("versioncheck", "Checks if your NPM or Bower dependencies are out of date.", function() {
+        var done = this.async(),
+            dependencyCalls = [];
+
+        var options = this.options({
+            skip : []
+        });
+
+        var allDependencies = componentFileToMetadatas("bower", readFile(grunt, "bower.json"))
+                      .concat(componentFileToMetadatas("npm",   readFile(grunt, "package.json")));
+
+        // Skip dependencies
+        allDependencies = _.reject(allDependencies, function(dependency) {
+            return _.contains(options.skip, dependency.name);
+        });
+
+        // NPM is really weird with logging. For some reason it wants to log all these
+        // command outputs to stdout. Unfortunately, this is a hack to supress that.
+        var log = console.log;
+        console.log = function() {};
+
+        _.each(allDependencies, function(dependency) {
+            var version = dependency.version;
+
+            switch (dependency.type) {
+                case "bower":
+                    dependencyCalls.push(function(callback) {
+                        bower.commands
+                            .info(dependency.name, "")
+                            .on("end", function(results) {
+                                var latest = results.versions[0];
+
+                                callback(null, _.merge({
+                                    latest : latest,
+                                    upToDate : semver.satisfies(latest, version)
+                                }, dependency));
+                            });
+                    });
+                    break;
+                case "npm":
+                    dependencyCalls.push(function(callback) {
+                        npm.commands.info([dependency.name, "version"], function(err, data) {
+                            // Data is structured as: { '1.2.1': { version: '1.2.1' } }
+                            // So get the first key of the object
+                            var latest = data[Object.keys(data)[0]].version;
+
+                            callback(null, _.merge({
+                                latest : latest,
+                                upToDate : semver.satisfies(latest, version)
+                            }, dependency));
+                        });
+                    });
+                    break;
+            }
+        });
+
+        npm.load({}, function() {
+            async.parallel(dependencyCalls, function(err, results) {
+                _.each(results, function(result) {
+                    if (!result.upToDate) {
+                        grunt.log.warn(result.name +
+                            (" (" + result.type + ")" + " is out of date. Your version: " +
+                                    result.version + " latest: " + result.latest).yellow);
+                    } else {
+                        grunt.log.ok(result.name + (" (" + result.type + ")" + " is up to date.").green);
+                    }
+                });
+                // Return the logger to its normal state
+                console.log = log;
+                done();
+            });
+        });
+
+    });
+
+};
